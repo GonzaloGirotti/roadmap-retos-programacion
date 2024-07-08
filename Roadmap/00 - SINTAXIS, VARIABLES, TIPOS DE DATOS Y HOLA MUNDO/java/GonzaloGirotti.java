8264169c798379a7719d28f087a0b19b9d8362a9
Gonzalo Girotti <gongirotti2001@gmail.com>
2024-07-08 12:14:35 -0300

    EJERCICIO #00 RUTA DE APRENDIZAJE JAVA

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/GonzaloGirotti.java
@@ -0,0 +1,52 @@
+// EJERICIO 1.a: ENUMERA TODOS LOS TIPOS DE COMENTARIOS, Y PON EN UNO DE ELLOS LA URL DE LA PAGINA OFICIAL DEL LENGUAJE QUE TRABAJAS (JAVA)
+
+/* 
+COMENTARIOS EN JAVA - TIPO 1: 
+EN BLOQUE 
+(MAS DE UNA LINEA) 
+*/
+
+// COMENTARIOS EN JAVA - TIPO 2: DE UNA LINEA
+
+/** COMENTARIOS EN JAVA - TIPO 3: JAVADOCS (TAMBIEN DE MAS DE UNA LINEA) 
+ 
+@author: GONZALO GIROTTI (EL @ SE UTILIZA CON UN IDENTIFICADOR, por ej: author, COMO UNA ETIQUETA)
+*/
+
+// java.com
+
+//=================================================================================================================================//
+
+//EJERCICIO 1.b: CREA UNA VARIABLE Y, DE SER POSIBLE, TAMBIEN UNA CONSTANTE
+
+//VARIABLE:
+int variable_uno;
+
+//CONSTANTE (SE LE ASIGNA VALOR QUE NO PUEDE SER MODIFICADO):
+final int constante_uno = 1;
+
+//=================================================================================================================================//
+
+//EJERCICIO 1.c: CREA UNA VARIABLE POR TIPO DE DATO PRIMITIVO
+
+//TIPOS NUMÉRICOS:
+    //ENTEROS:
+        int numero_entero;
+        short numero_corto;
+        long numero_largo;
+        byte numero_byte;
+    //CON COMA:
+        float numero_flotante;
+        double numero_decimal;
+//BOOLEANOS:
+        boolean booleano_logico;
+//CARACTERES:
+        char caracter;
+//CADENAS DE TEXTO:
+        String cadena_texto; //NO ES PRIMITIVO, PERO ES UN TIPO DE DATO IMPORTANTE
+
+
+    
+
+
+
